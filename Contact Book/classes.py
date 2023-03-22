0fc8a687c34460dfe94ccb86e93d4c0c95143a39
AmyStas <95294347+amystas@users.noreply.github.com>
2023-03-22 10:06:02 +0100

    changed class variables to private

--- Contact Book/classes.py
+++ Contact Book/classes.py
@@ -10,33 +10,33 @@ def checkEmailRegEx(text):
 class Contact:
     def __init__(self, name='', surname='', company='', phone='', email=''):
         if name.isalpha() and surname.isalpha():
-            self.name = name
-            self.surname = surname
-            self.company = company
+            self.__name = name
+            self.__surname = surname
+            self.__company = company
         if phone.isdigit() or phone == '':
-            self.phone = phone
+            self.__phone = phone
         else:
             print('Invalid phone number')
-            self.phone = ''
+            self.__phone = ''
         if checkEmailRegEx(email) or email=='':
-            self.email = email
+            self.__email = email
         else:
             print('Invalid email')
-            self.email = ''
-        self.isFavorite = False
+            self.__email = ''
+        self.__isFavorite = False
 
     def __str__(self):
         personalData = {
-            "Name": self.name,
-            "Surname": self.surname,
-            "Company": self.company,
-            "Phone number": self.phone,
-            "Email address": self.email
+            "Name": self.__name,
+            "Surname": self.__surname,
+            "Company": self.__company,
+            "Phone number": self.__phone,
+            "Email address": self.__email
         }
         return tabulate(personalData.items(), tablefmt="simple_grid")
 
     def __repr__(self):
-        return self.surname
+        return self.__surname
     
     def setFromList(self, givenList):
         if len(givenList) > 5:
@@ -46,46 +46,46 @@ class Contact:
             fillingList = ['' for i in range(blankFields)]
             givenList.extend(fillingList)
 
-        self.name = givenList[0]
-        self.surname = givenList[1]
-        self.company = givenList[2]
-        self.phone = givenList[3]
-        self.email = givenList[4]
+        self.__name = givenList[0]
+        self.__surname = givenList[1]
+        self.__company = givenList[2]
+        self.__phone = givenList[3]
+        self.__email = givenList[4]
 
     def getAsList(self):
-        return [self.name, self.surname, self.company, self.phone, self.email]
+        return [self.name, self.__surname, self.__company, self.__phone, self.__email]
 
     def get(self, attribute):
         match attribute:
             case 'name':
-                return self.name
+                return self.__name
             case 'surname':
-                return self.surname
+                return self.__surname
             case 'company':
-                return self.company
+                return self.__company
             case 'phone':
-                return self.phone
+                return self.__phone
             case 'email':
-                return self.email
+                return self.__email
             case other:
                 print('There is no such attribute')
     
     def set(self, attribute, value):
         match attribute:
             case 'name':
-                self.name = value
+                self.__name = value
             case 'surname':
-                self.surname = value
+                self.__surname = value
             case 'company':
-                self.company = value
+                self.__company = value
             case 'phone':
                 if value.isdigit():
-                    self.phone = value
+                    self.__phone = value
                 else: 
                     return False
             case 'email':
                 if checkEmailRegEx(value):
-                    self.email = value
+                    self.__email = value
                 else:
                     return False
             case other:
@@ -98,20 +98,20 @@ class Contacts:
 
     def __init__(self, givenList=None):
         if givenList is None:
-            self.listOfContacts = []
+            self.__listOfContacts = []
         else:
-            self.listOfContacts = givenList.copy()
+            self.__listOfContacts = givenList.copy()
 
     def __str__(self):
-        if self.listOfContacts:
+        if self.__listOfContacts:
             theHeader = ['NAME', 'SURNAME', 'COMPANY', 'PHONE NUMBER', 'EMAIL ADDRESS']
             
-            listOfFavorites = [prs for prs in self.listOfContacts if prs.isFavorite]
+            listOfFavorites = [prs for prs in self.__listOfContacts if prs.isFavorite]
             if listOfFavorites: 
                 listOfFavorites = sorted(listOfFavorites, key=lambda x: x.surname)
                 listOfFavorites = [prs.getAsList() for prs in listOfFavorites]
             
-            listOfItems = [prs for prs in self.listOfContacts if prs.isFavorite == False]
+            listOfItems = [prs for prs in self.__listOfContacts if prs.isFavorite == False]
             listOfItems = sorted(listOfItems, key=lambda x: x.surname)
             listOfItems = [prs.getAsList() for prs in listOfItems]
             
@@ -129,23 +129,23 @@ class Contacts:
         return self
     
     def __next__(self):
-        if self.i >= len(self.listOfContacts):
+        if self.i >= len(self.__listOfContacts):
             raise StopIteration
-        value = self.listOfContacts[self.i]
+        value = self.__listOfContacts[self.i]
         self.i += 1
         return value
 
     def search(self, givenContact):
         try:
-            position = self.listOfContacts.index(givenContact)
-            #print('Found at position: ' + str(position+1) + ' out of ' + str(len(self.listOfContacts)))
+            position = self.__listOfContacts.index(givenContact)
+            #print('Found at position: ' + str(position+1) + ' out of ' + str(len(self.__listOfContacts)))
             return position
         except ValueError:
             return None
             
     def searchFor(self, searchingBy, searchedValue):
         found = []
-        for contact in self.listOfContacts:
+        for contact in self.__listOfContacts:
             if contact.get(searchingBy) == searchedValue:
                 found.append(contact)
         if found:
@@ -155,13 +155,13 @@ class Contacts:
 
     def add(self, givenContact):
         if isinstance(givenContact, Contact):
-            self.listOfContacts.append(givenContact)
+            self.__listOfContacts.append(givenContact)
             return True
         else:
             return False
 
     def pop(self, index = -1):
-        self.listOfContacts.pop(index)
+        self.__listOfContacts.pop(index)
 
     def addAtPosition(self, givenContact, givenIndex):
-        self.listOfContacts.insert(givenIndex, givenContact)
+        self.__listOfContacts.insert(givenIndex, givenContact)
